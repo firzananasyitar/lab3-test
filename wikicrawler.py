9a384137aa58a2b5ca81856ba29087e3ea7e162c
firzananasyitar <61526913+firzananasyitar@users.noreply.github.com>
2020-03-06 20:16:18 +0800

    Add files via upload

---
+++ wikicrawler.py
@@ -0,0 +1,12 @@
+import wikipedia
+import sys
+
+
+page = wikipedia.page(sys.argv[-1])
+links = []
+
+for link in page.links:
+	links.append({"page":link})
+	
+for link in links:
+	print link["page"]
